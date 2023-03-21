d7f75abef9640fed5cba0e0341261dd55103b0ed
Andres Howard <andreshoward@MacBook-Pro-de-Andres.local>
2023-03-21 15:59:10 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from rectangle import Rectangle
+
+my_rectangle = Rectangle(3, 4)
+print(my_rectangle)
---
+++ rectangle.py
@@ -0,0 +1,12 @@
+from figure import Figure
+
+class Rectangle(Figure):
+    def __init__(self, height, width):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return self.width * self.height
+    
+    def perimeter(self):
+        return 2 * (self.width + self.height)
